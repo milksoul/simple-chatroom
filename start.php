216d24b2c8f8dbfa4b96b00521d256f60687b530
milksoul <1989rui422@163.com>
2018-07-14 17:21:58 +0800

    text协议demo

--- start.php
+++ start.php
@@ -1,3 +1,33 @@
 <?php
+use Workerman\Worker;
 
-phpinfo();
+require_once './vendor/autoload.php';
+
+$global_uid = 0;
+function handle_connection($connection){
+    global $text_workder, $global_uid;
+    $connection->uid = ++$global_uid;
+}
+
+function handle_message($connection, $data){
+    global $text_worker;
+    foreach($text_worker->connections as $conn){
+        $conn->send('user '.$connection->uid.' said:'.$data);
+    }
+}
+
+function handle_close($connection){
+    global $text_workder;
+    foreach ($text_workder->connections as $conn) {
+        $conn->send('user '.$connection->uid.' logout');
+    }
+}
+
+$text_worker = new Worker('text://0.0.0.0:1743422');
+$text_worker->count = 1;
+
+$text_worker->onConnect = 'handle_connection';
+$text_worker->onMessage = 'handle_message';
+$text_worker->onClose = 'handle_close';
+
+Worker::runAll();
